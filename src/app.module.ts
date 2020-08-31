55a96d653138fa56556d8e4d30f96bb3eaf45457
Aline Freitas <aline.freitas@thoughtworks.com>
2020-08-31 14:32:54 -0300

    Remove dotenv e utiliza módulo de nestjs/config para fazer interface com arquivo .env (@AlineFreitas)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GiphyModule } from './giphy/giphy.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [GiphyModule],
+  imports: [GiphyModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
